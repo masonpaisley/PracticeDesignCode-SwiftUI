5417e002dfc488dcc9cba7d3838eff1eb3d132f0
masonpaisley <masonpaisleyy@gmail.com>
2022-07-31 00:15:21 +0800

    Initial Commit

---
+++ Shared/PracticeDesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeDesignCodeApp.swift
+//  Shared
+//
+//  Created by jinzhao wang on 2022/7/31.
+//
+
+import SwiftUI
+
+@main
+struct PracticeDesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
